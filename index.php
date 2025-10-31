c359bbdcdd73914e123479dcb469e4586f4a3966
Rodrigo <rodrigo@rodrigoti.com.br>
2025-10-30 23:51:48 -0300

    final

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>DataTrends - Em Manutenção</title>
+    <title>DataTrends - Em Manutenção </title>
     <link rel="icon" type="image/png" href="favicon.png">
     <style>
         * {
